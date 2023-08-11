09f3f232944c21f2dd27543db5fe68e3c81809c7
IannOli <100435787+iannoliver@users.noreply.github.com>
2023-08-11 10:46:25 -0300

    Update script.js

--- front-paperbook/Endereco/js/script.js
+++ front-paperbook/Endereco/js/script.js
@@ -1,13 +1,20 @@
 //referenciar os controles: input nome, descricao e btncadastrar
-let idcat = ""
-let nome = document.getElementById("nome")
-let descricao = document.getElementById("descricao")
+let idend = ""
+let tipo = document.getElementById("tipo")
+let logradouro = document.getElementById("logradouro")
+let cep = document.getElementById("cep")
+let numero = document.getElementById("numero")
+let complemento = document.getElementById("complemento")
+let referencia = document.getElementById("referencia")
+let bairro = document.getElementById("bairro")
+let cidade = document.getElementById("cidade")
+let estado = document.getElementById("estado")
 let btncadastrar = document.getElementById("btncadastrar")
 
 //Realizar o cadastro ou a atualização quando o botão for pressionado
 btncadastrar.onclick = () => {
     if(btncadastrar.value == "Atualizar") {
-        fetch("http://10.26.44.57:5000/api/v1/categoria/atualizar/"+idcat, {
+        fetch("http://10.26.44.57:5000/api/v1/endereco/atualizar/"+idend, {
             method:"PUT",
             headers:{
                 "accept":"application/json",
@@ -21,10 +28,10 @@ btncadastrar.onclick = () => {
         .then((response)=>response.json())
         .then((dados)=>alert("Atualizado")) 
         .catch((error)=>console.error(error))
-        alert("A Categoria foi atualizada. Atualize a página")
+        alert("O Endereço foi atualizado. Atualize a página")
         window.location.reload()
     } else {
-        fetch("http://10.26.44.57:5000/api/v1/categoria/cadastrar", {
+        fetch("http://10.26.44.57:5000/api/v1/endereco/cadastrar", {
         method:"POST",
         headers:{
             "accept":"application/json",
@@ -38,28 +45,36 @@ btncadastrar.onclick = () => {
     .then((response)=>response.json())
     .then((dados)=>alert(dados)) 
     .catch((error)=>console.error(error))
-    alert("A Categoria foi criada. Atualize a página")
+    alert("O Endereço foi cadastrado. Atualize a página")
     window.location.reload()
     }
 }
 
 // exibir as catecorias cadastradas
-function exibirCategorias() {
+function exibirEnderecos() {
     let saida = ""
-    fetch("http://10.26.44.57:5000/api/v1/categoria/listar")
+    fetch("http://10.26.44.57:5000/api/v1/endereco/listar")
     .then((response)=>response.json())
     .then((dados) => {
         dados.map((itens,ix)=>{
-            saida += `<tr><td>${itens.idcategoria}</td>
-            <td>${itens.nomecategoria}</td>
-            <td>${itens.descricaocategoria}</td>
+            saida += `<tr><td>${itens.idendereco}</td>
+            <td>${itens.tipo}</td>
+            <td>${itens.logradouro}</td>
+            <td>${itens.cep}</td>
+            <td>${itens.numero}</td>
+            <td>${itens.complemento}</td>
+            <td>${itens.referencia}</td>
+            <td>${itens.bairro}</td>
+            <td>${itens.cidade}</td>
+            <td>${itens.estado}</td>
+                        
             <td>
-                <a href=# onclick="atualizar('${itens.idcategoria}','${itens.nomecategoria}','${itens.descricaocategoria}')">
+                <a href=# onclick="atualizar('${itens.idendereco}','${itens.tipo}','${itens.logradouro}', '${itens.cep}', '${itens.numero}', '${itens.complemento}', '${itens.referencia}', '${itens.bairro}', '${itens.cidade}', '${itens.estado}')">
                 Atualizar
                 </a>
             </td>
             <td> 
-                <a href=# onclick=apagar('${itens.idcategoria}')>
+                <a href=# onclick=apagar('${itens.idendereco}')>
                 Apagar
                 </a>
             </td>
@@ -69,10 +84,17 @@ function exibirCategorias() {
     }).catch((error)=>console.error("Erro na api "+error))
 }
 
-function atualizar(id,cat, desc){
-    idcat = id    
-    nome.value = cat
-    descricao.value = desc
+function atualizar(id, tipo, log, cep, num, comp, ref, bairro, cidade, estado){
+    idend = id    
+    tipo.value = tipo
+    logradouro.value = log
+    cep.value = cep
+    numero.value = num
+    complemento.value = comp
+    referencia.value = ref
+    bairro.value = bairro
+    cidade.value = cidade
+    estado.value = estado
 
     document.getElementById("btncadastrar").value = "Atualizar"
 
@@ -80,12 +102,12 @@ function atualizar(id,cat, desc){
 }
 
 function apagar(id){
-    fetch("http://10.26.44.57:5000/api/v1/categoria/apagar/"+id, {method:"DELETE"})
+    fetch("http://10.26.44.57:5000/api/v1/endereco/apagar/"+id, {method:"DELETE"})
     .then((response)=>response.json())
     .then((dados) => {
         
     }
     ).catch((error)=>console.error("Erro de aplicação"+error))
-    alert("A Categoria foi apagada. Atualize a página")
+    alert("O Endereç foi apagado. Atualize a página")
     window.location.reload()
 }
